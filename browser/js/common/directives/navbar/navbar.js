9fcd62432d4beb9eaab4f7f62e32b4c4894efebe
Ten <tenloh@gmail.com>
2016-11-14 14:12:31 -0500

    new layout

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -3,9 +3,8 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
     return {
         restrict: 'E',
         scope: {},
-        // templateUrl: 'js/common/directives/navbar/navbar.html',
+        templateUrl: 'js/common/directives/navbar/navbar.html',
         link: function (scope) {
-
             scope.items = [
                 { label: 'Home', state: 'home' },
                 { label: 'About Us', state: 'about' },
